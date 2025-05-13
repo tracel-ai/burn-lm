00623f2147ee7a1943f776875c250b75db538fdf
nathaniel <nathaniel.simard.42@gmail.com>
2025-05-13 12:52:45 -0400

    Optimize cache

--- models/llama-burn/src/cache.rs
+++ models/llama-burn/src/cache.rs
@@ -3,7 +3,7 @@ use burn::tensor::{backend::Backend, Device, Tensor};
 #[derive(Debug, Clone)]
 pub(crate) struct AutoregressiveCache<B: Backend> {
     /// Tensor cache with shape `[batch_size, num_heads, seq_len, d_model]`
-    cache: Tensor<B, 4>,
+    cache: Option<Tensor<B, 4>>,
     pub(crate) max_seq_len: usize,
     cur_seq_len: usize,
 }
@@ -18,7 +18,10 @@ impl<B: Backend> AutoregressiveCache<B> {
         device: &Device<B>,
     ) -> Self {
         Self {
-            cache: Tensor::empty([max_batch_size, num_heads, max_seq_len, d_model], device),
+            cache: Some(Tensor::empty(
+                [max_batch_size, num_heads, max_seq_len, d_model],
+                device,
+            )),
             max_seq_len,
             cur_seq_len: 0,
         }
@@ -26,30 +29,38 @@ impl<B: Backend> AutoregressiveCache<B> {
 
     /// Reset the cache state.
     pub fn reset(&mut self) {
-        self.cache = Tensor::empty(self.cache.shape(), &self.cache.device());
+        let old_cache = self.cache.take().expect("Cache to be allocated");
+        let shape = old_cache.shape();
+        let device = old_cache.device();
+
+        core::mem::drop(old_cache);
+
+        self.cache = Some(Tensor::empty(shape, &device));
         self.cur_seq_len = 0;
     }
 
     pub fn forward(&mut self, tensor: Tensor<B, 4>) -> Tensor<B, 4> {
+        let mut cache = self.cache.take().expect("Cache to be allocated");
+
         let [batch_size, num_heads, seq_len, d_model] = tensor.dims();
         let mut new_seq_len = self.cur_seq_len + seq_len;
 
         if new_seq_len > self.max_seq_len {
             self.cur_seq_len = self.max_seq_len - seq_len;
-            let prev_slice = self.cache.clone().slice([
+            let prev_slice = cache.clone().slice([
                 0..batch_size,
                 0..num_heads,
                 seq_len..self.max_seq_len,
                 0..d_model,
             ]);
-            self.cache = self.cache.clone().slice_assign(
+            cache = cache.slice_assign(
                 [0..batch_size, 0..num_heads, 0..self.cur_seq_len, 0..d_model],
                 prev_slice,
             );
             new_seq_len = self.max_seq_len;
         }
 
-        self.cache = self.cache.clone().slice_assign(
+        cache = cache.slice_assign(
             [
                 0..batch_size,
                 0..num_heads,
@@ -61,9 +72,14 @@ impl<B: Backend> AutoregressiveCache<B> {
 
         self.cur_seq_len += seq_len;
 
-        self.cache
-            .clone()
-            .slice([0..batch_size, 0..num_heads, 0..self.cur_seq_len, 0..d_model])
+        let result =
+            cache
+                .clone()
+                .slice([0..batch_size, 0..num_heads, 0..self.cur_seq_len, 0..d_model]);
+
+        self.cache = Some(cache);
+
+        result
     }
 
     /// Returns the cached sequence length.
